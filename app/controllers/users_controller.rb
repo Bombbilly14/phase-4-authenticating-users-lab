7ddf97ede94da9b97e223b2a7c856410492bbb10
William German <william.j.german2@gmail.com>
2023-01-21 09:41:39 -0800

    commit tree dbbb1a03a8ae7cf1e2c51eeb15b96c76f9c44979

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+    ##staying logged in
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+          render json: user
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+## staying logged in below
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
